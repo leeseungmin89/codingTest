80733b066ad795f0c96c3e90e40e890cdb03629f
leeseungmin89 <124274426+leeseungmin89@users.noreply.github.com>
2023-04-28 23:14:36 +0900

    [level 0] Title: 짝수의 합, Time: 0.21 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if (n > 0 && n <= 1000) {
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 == 0) {
+                    answer += i;
+                }
+            }
+        }
+        return answer;
+    }
+}
